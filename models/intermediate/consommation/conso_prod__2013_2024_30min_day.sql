bfb856e3294dbb18928da1849139df5c5a4c86ba
Roxane Thomoux <roxane.thomoux@gmail.com>
2024-08-20 15:37:02 +0000

    ajout de la colonne pop au tableau join de conso prod de 2013 à 2024

--- models/intermediate/consommation/conso_prod__2013_2024_30min_day.sql
+++ models/intermediate/consommation/conso_prod__2013_2024_30min_day.sql
@@ -1,6 +1,20 @@
 select 
         date_date,
         region,
+       CASE 
+            WHEN region = "Île-de-France" THEN 12419961
+            WHEN region = "Auvergne-Rhône-Alpes" THEN 8235923
+            WHEN region = "Hauts-de-France" THEN 5983823
+            WHEN region = "Nouvelle-Aquitaine" THEN 6154772
+            WHEN region = "Grand Est" THEN 5568711
+            WHEN region = "Occitanie" THEN 6154729
+            WHEN region = "Provence-Alpes-Côte d'Azur" THEN 5198011
+            WHEN region = "Normandie'" THEN 3327077
+            WHEN region = "Pays de la Loire" THEN 3926389
+            WHEN region = "Bretagne" THEN 3453023
+            WHEN region = "Bourgogne-Franche-Comté" THEN 2791719
+            WHEN region = "Centre-Val de Loire" THEN 2573295
+        END AS population, 
         ROUND(avg(consommation_mw),2) AS consommation_mw,
         ROUND(avg(thermique_mw),2) AS thermique_mw,
         ROUND(avg(nucleaire_mw),2) AS nucleaire_mw,
